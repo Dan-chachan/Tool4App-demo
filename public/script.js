d45db7a1ff2f52541f4861ea88e951b6ec8e0194
Dan-chan <T0xicCl0ud@hotmail.com>
2019-04-05 14:27:01 +0200

    Botton bar now displays fun info

--- public/script.js
+++ public/script.js
@@ -14,7 +14,16 @@ food_amounts.forEach(function(amountElem) {
 
     btnLess.addEventListener("click", function() {
         var oldnum = amountElem.innerHTML;
+        amountElem.pare
         var newnum = parseInt(oldnum) - 1
         amountElem.innerHTML = (newnum > 0) ? newnum : 0;
     }) 
 })
+
+function getPrice(elem) {
+    var parent = elem.parentNode;
+    var foodinfo = parent.nextSibling;
+    var price = foodinfo.getElementsByClassName("foodprice")
+
+    console.log(price)
+}
